c0f0bc4b0d747ff1b2d78b427573d871f884660b
Ludovic Roche <ludovic.roche@canaltp.fr>
2016-04-11 10:57:59 +0200

    Origin field added in response

--- Entity/UserRepository.php
+++ Entity/UserRepository.php
@@ -7,11 +7,16 @@ use InvalidArgumentException;
 
 class UserRepository extends EntityRepository
 {
-    public function findUsersBetweenDates(\DateTime $startDate, \DateTime $endDate, $sortField = 'id', $sortOrder = 'asc')
-    {
+    public function findUsersBetweenDates(
+        \DateTime $startDate,
+        \DateTime $endDate,
+        $sortField = 'id',
+        $sortOrder = 'asc'
+    ) {
         if (!in_array($sortOrder, array('asc', 'desc'))) {
             throw new InvalidArgumentException('Sort order argument must be \'asc\' or \'desc\'');
         }
+
         $query = $this->createQueryBuilder('u')
             ->where('u.createdAt >= :start_date')
             ->andWhere('u.createdAt <= :end_date')
